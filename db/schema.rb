c96317d853971e277c82508724110c91e3a3900e
Mayra Moreno <maleja13p@gmail.com>
2022-09-22 21:05:55 -0500

    primer modelo, falta front

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_21_011128) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_23_012343) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "movies", force: :cascade do |t|
-    t.string "title"
+  create_table "lists", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
